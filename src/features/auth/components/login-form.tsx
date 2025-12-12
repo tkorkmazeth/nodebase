d6bdb0d0157732b47a50962d3a036ed6b2ea5b77
Muhammed Talha Korkmaz <kriptovekediler@gmail.com>
2025-12-12 22:26:04 +0300

    theme & styling

--- src/features/auth/components/login-form.tsx
+++ src/features/auth/components/login-form.tsx
@@ -83,6 +83,12 @@ export function LoginForm() {
                     type="button"
                     disabled={isPending}
                   >
+                    <Image
+                      src="/logos/github.svg"
+                      alt="Github"
+                      width={20}
+                      height={20}
+                    />
                     Continue with Github
                   </Button>
 
@@ -92,6 +98,12 @@ export function LoginForm() {
                     type="button"
                     disabled={isPending}
                   >
+                    <Image
+                      src="/logos/google.svg"
+                      alt="Google"
+                      width={20}
+                      height={20}
+                    />
                     Continue with Google
                   </Button>
                 </div>
